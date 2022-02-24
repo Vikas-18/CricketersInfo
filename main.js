d259ea2d0d2394b540940a9195ee594267a7084e
Vikas-18 <vikaskohli18impossible@gmail.com>
2022-02-24 18:28:26 +0530

    bold text added

--- main.js
+++ main.js
@@ -37,59 +37,59 @@ form.addEventListener("submit", function getname(e) {
 
           let name = document.getElementById("name");
           name.innerHTML = "";
-          name.innerHTML = `Full Name: ${data.name}`;
+          name.innerHTML = `<b>Full Name:</b> ${data.name}`;
 
           let dob = document.getElementById("dob");
           dob.innerHTML = "";
-          dob.innerHTML = `DOB: ${data.DoB}`;
+          dob.innerHTML = `<b>DOB:</b> ${data.DoB}`;
 
           let birthplace = document.getElementById("birthplace");
           birthplace.innerHTML = "";
-          birthplace.innerHTML = `Birth Place: ${data.birthPlace}`;
+          birthplace.innerHTML = `<b>Birth Place:</b> ${data.birthPlace}`;
 
           let bat = document.getElementById("bat");
           bat.innerHTML = "";
-          bat.innerHTML = `Bat: ${data.bat}`;
+          bat.innerHTML = `<b>Bat:</b> ${data.bat}`;
 
           let bowl = document.getElementById("bowl");
           bowl.innerHTML = "";
-          bowl.innerHTML = `Bowl: ${data.bowl}`;
+          bowl.innerHTML = `<b>Bowl:</b> ${data.bowl}`;
 
           let height = document.getElementById("height");
           height.innerHTML = "";
-          height.innerHTML = `Height: ${data.height}`;
+          height.innerHTML = `<b>Height:</b> ${data.height}`;
 
           let team = document.getElementById("team");
           team.innerHTML = "";
-          team.innerHTML = `Teams: ${data.teams[0]}`;
+          team.innerHTML = `<b>Teams:</b> ${data.teams[0]}`;
 
           let batodirank = document.getElementById("batodirank");
           batodirank.innerHTML = "";
-          batodirank.innerHTML = `Rank(Bat): Best(ODI): ${data.currRank.bat.odiBestRank} Current Rank: ${data.currRank.bat.odiRank}`;
+          batodirank.innerHTML = `<b>Rank(Bat):</b> <b>Best(ODI):</b> ${data.currRank.bat.odiBestRank} <b>Current Rank:</b> ${data.currRank.bat.odiRank}`;
 
           let batt20rank = document.getElementById("batt20rank");
           batt20rank.innerHTML = "";
-          batt20rank.innerHTML = `Rank(Bat): Best(T20): ${data.currRank.bat.t20BestRank} Current Rank: ${data.currRank.bat.t20Rank}`;
+          batt20rank.innerHTML = `<b>Rank(Bat):</b> <b>Best(T20):</b> ${data.currRank.bat.t20BestRank} <b>Current Rank:</b> ${data.currRank.bat.t20Rank}`;
 
           let battestrank = document.getElementById("battestrank");
           battestrank.innerHTML = "";
-          battestrank.innerHTML = `Rank(Bat): Best(Test): ${data.currRank.bat.testBestRank} Current Rank: ${data.currRank.bat.testRank}`;
+          battestrank.innerHTML = `<b>Rank(Bat):</b> <b>Best(Test):</b> ${data.currRank.bat.testBestRank} <b>Current Rank:</b> ${data.currRank.bat.testRank}`;
 
           let bowlodirank = document.getElementById("bowlodirank");
           bowlodirank.innerHTML = "";
-          bowlodirank.innerHTML = `Rank(Bowl): Best(ODI): ${data.currRank.bowl.odiBestRank} Current Rank: ${data.currRank.bowl.odiRank}`;
+          bowlodirank.innerHTML = `<b>Rank(Bowl):</b> <b>Best(ODI):</b> ${data.currRank.bowl.odiBestRank} <b>Current Rank:</b> ${data.currRank.bowl.odiRank}`;
 
           let bowlt20rank = document.getElementById("bowlt20rank");
           bowlt20rank.innerHTML = "";
-          bowlt20rank.innerHTML = `Rank(Bowl): Best(T20): ${data.currRank.bowl.t20BestRank} Current Rank: ${data.currRank.bowl.t20Rank}`;
+          bowlt20rank.innerHTML = `<b>Rank(Bowl): </b><b>Best(T20):</b> ${data.currRank.bowl.t20BestRank} <b>Current Rank:</b> ${data.currRank.bowl.t20Rank}`;
 
           let bowltestrank = document.getElementById("bowltestrank");
           bowltestrank.innerHTML = "";
-          bowltestrank.innerHTML = `Rank(Bowl): Best(Test): ${data.currRank.bowl.testBestRank} Current Rank: ${data.currRank.bowl.testRank}`;
+          bowltestrank.innerHTML = `<b>Rank(Bowl):</b> <b>Best(Test):</b> ${data.currRank.bowl.testBestRank} <b>Current Rank:</b> ${data.currRank.bowl.testRank}`;
 
           let bio = document.getElementById("bio");
           bio.innerHTML = "";
-          bio.innerHTML = `Bio: ${data.bio}`;
+          bio.innerHTML = `<b>Biography:</b> ${data.bio}`;
         });
     });
 });
